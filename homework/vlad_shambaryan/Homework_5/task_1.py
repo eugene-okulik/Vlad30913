0ee3504aecc43d275427c50160498be84a1cad95
PC <skachat30913@gmail.com>
2024-07-09 14:03:38 +0400

    update

--- homework/vlad_shambaryan/Homework_5/task_1.py
+++ homework/vlad_shambaryan/Homework_5/task_1.py
@@ -1,13 +1,3 @@
-# Задание 1
-# Дан такой список:
-#
-# person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
-#
-# С помощью распаковки создайте из этого списка переменные, содержащие соответствующие данные:
-#
-# name, last_name, city, phone, country
-
-
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 
 name, last_name, city, phone, country = person  # распаковка списка в отдельные переменные
--- homework/vlad_shambaryan/Homework_5/task_3.py
+++ homework/vlad_shambaryan/Homework_5/task_3.py
@@ -1,15 +1,3 @@
-# Задание 3
-# Даны такие списки:
-#
-# students = ['Ivanov', 'Petrov', 'Sidorov']
-#
-# subjects = ['math', 'biology', 'geography']
-#
-# Распечатайте текст, который будет использовать данные из этих списков. Текст в итоге должен выглядеть так:
-#
-# Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography
-
-
 students = ['Ivanov', 'Petrov', 'Sidorov']  # имена студентов
 
 subjects = ['math', 'biology', 'geography']
