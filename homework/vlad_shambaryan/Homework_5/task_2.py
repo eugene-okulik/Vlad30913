0c5bb9449a674b4afc94ee5eedbcc22f1dd28949
PC <skachat30913@gmail.com>
2024-07-09 20:35:09 +0400

    update task 2

--- homework/vlad_shambaryan/Homework_5/task_2.py
+++ homework/vlad_shambaryan/Homework_5/task_2.py
@@ -3,8 +3,8 @@ result2 = "результат операции: 514"
 result3 = "результат работы программы: 9"
 
 number1 = int(result1[result1.index(':') + 2:]) + 10  # находит индекс символа :  пропуская двоеточие и пробел
-number2 = int(result2[result2.index(':') + 2:]) + 10
-number3 = int(result3[result3.index(':') + 2:]) + 10
+number2 = int(result2[result2.index(':') + 2:]) + 10  # находит индекс символа :  пропуская двоеточие и пробел
+number3 = int(result3[result3.index(':') + 2:]) + 10  # находит индекс символа :  пропуская двоеточие и пробел
 
 print(number1)
 print(number2)
