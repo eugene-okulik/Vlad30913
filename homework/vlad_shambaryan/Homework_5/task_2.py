e989d413a4aa25daa384aa5fc349408bed058add
PC <skachat30913@gmail.com>
2024-07-09 21:08:51 +0400

    update task 2

--- homework/vlad_shambaryan/Homework_5/task_2.py
+++ homework/vlad_shambaryan/Homework_5/task_2.py
@@ -2,10 +2,10 @@ result1 = "результат операции: 42"
 result2 = "результат операции: 514"
 result3 = "результат работы программы: 9"
 
-number1 = int(result1[result1.index(':') + 2:]) + 10  # находит индекс символа :  пропуская двоеточие и пробел
-number2 = int(result2[result2.index(':') + 2:]) + 10  # находит индекс символа :  пропуская двоеточие и пробел
-number3 = int(result3[result3.index(':') + 2:]) + 10  # находит индекс символа :  пропуская двоеточие и пробел
+number1 = int(result1[result1.index(':') + 2:])
 
-print(number1)
-print(number2)
-print(number3)
+number2 = int(result2[result2.index(':') + 2:])
+
+number3 = int(result3[result3.index(':') + 2:])
+
+print(number1 + 10, number2 + 10, number3 + 10)
