4579a4d3c5a08770b037538926f203295e1ec717
PC <skachat30913@gmail.com>
2024-07-10 23:17:16 +0400

    update

--- homework/vlad_shambaryan/Homework_6/task_1.py
+++ homework/vlad_shambaryan/Homework_6/task_1.py
@@ -1,5 +1,6 @@
 
-text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel. " +
+        "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
 
 words = text.split()  # разбиваем текст на слова с сохранением знаков препинания
 
