3e32644eb7f9d6d2533567f9d0d6e00c82195117
PC <skachat30913@gmail.com>
2024-07-10 23:20:39 +0400

    update

--- homework/vlad_shambaryan/Homework_6/task_1.py
+++ homework/vlad_shambaryan/Homework_6/task_1.py
@@ -1,16 +1,20 @@
-
 text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel. " +
         "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
 
-words = text.split()  # разбиваем текст на слова с сохранением знаков препинания
+words = text.split()
 
 new_words = []
 
-for word in words:  # проходим по каждому слову
-    if word[-1] in ',.':  # проверяем, если слово заканчивается на запятую или точку
-        new_word = word[:-1] + 'ing' + word[-1]  # добавляем 'ing' перед знаком препинания
+for word in words:
+
+    if word[-1] in ',.':
+
+        new_word = word[:-1] + 'ing' + word[-1]
+
     else:
+
         new_word = word + 'ing'
+
     new_words.append(new_word)
 
 new_text = ' '.join(new_words)  # объединяем слова обратно в строку
