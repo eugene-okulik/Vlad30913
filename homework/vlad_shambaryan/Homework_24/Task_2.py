57e3c9cc827afef7c1490f57eb084773fba63125
PC <skachat30913@gmail.com>
2024-08-29 18:59:31 +0400

    task1, 2

--- homework/vlad_shambaryan/Homework_24/Task_2.py
+++ homework/vlad_shambaryan/Homework_24/Task_2.py
@@ -1,35 +1,31 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+import pytest
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-import pytest
 
 
 @pytest.fixture()
 def driver():
     chrome_driver = webdriver.Chrome()
     chrome_driver.maximize_window()
-    chrome_driver.implicitly_wait(5)
     yield chrome_driver
 
 
-def test_product(driver):
-    driver.get("https://magento.softwaretestingboard.com/gear/bags.html")
-    item = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
-        (By.XPATH, "(//a[@class='product-item-link'])[1]")
-    ))
-    item_name = item.find_element(By.XPATH, "(//a[@class='product-item-link'])").text
+def test_Products(driver):
+    driver.get('https://magento.softwaretestingboard.com/gear/bags.html')
+    item_name = driver.find_element(By.XPATH, "(//a[@class='product-item-link'])[1]").text
+    item = driver.find_element(By.XPATH,  "//img[@alt='Push It Messenger Bag']")
+    add_compare = driver.find_element(By.XPATH, "(//a[contains(.,'Add to Compare')])[1]")
     actions = ActionChains(driver)
     actions.move_to_element(item)
-    add_to_compare_btn = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
-        (By.XPATH, "(//a[@class='action tocompare'])"))
-    )
-    actions.move_to_element(add_to_compare_btn).click().perform()
-    WebDriverWait(driver, 5).until(EC.presence_of_element_located(
-        (By.XPATH, "(//div[@class='block block-compare'])"))
-    )
-    added_item = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
-        (By.XPATH, "(//strong[@class='product-item-name'])"))
-    ).text
+    actions.click(add_compare)
+    actions.perform()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        (By.XPATH, "//div[@class='block block-compare']")))
+    added_item = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "(//strong[@class='product-item-name'])"))).text
     assert added_item in item_name
+
