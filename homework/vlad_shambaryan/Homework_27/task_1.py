30617b5ada7806e8296f2fab24290ffb1a8aebaf
PC <skachat30913@gmail.com>
2024-09-12 16:56:34 +0400

    playwrite

--- homework/vlad_shambaryan/Homework_27/task_1.py
+++ homework/vlad_shambaryan/Homework_27/task_1.py
@@ -1,12 +1,12 @@
 from playwright.sync_api import Page, expect
 
 
-def test_confirm_alert8(page: Page):
+def test_confirm_alert(page: Page):
     page.goto("https://www.qa-practice.com/elements/alert/confirm")
     page.on("dialog", lambda dialog: dialog.accept())
     button = page.locator("//a[@class='a-button']")
     button.click()
-    confirmation = page.locator("//div[contains(@id,'result')]")
-    expect(confirmation).to_be_visible()
-    confirmation_text = confirmation.inner_text().strip().replace('\n', ' ').replace('  ', ' ')
-    assert confirmation_text == "You selected Ok"
+    verification = page.locator("//div[contains(@id,'result')]")
+    expect(verification).to_be_visible()
+    verification_text = verification.inner_text().strip().replace('\n', ' ').replace('  ', ' ')
+    assert verification_text == "You selected Ok"
