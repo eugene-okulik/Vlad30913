2d99181a1d77e406a513c114746f2d93b662b278
PC <skachat30913@gmail.com>
2024-09-12 22:11:01 +0400

    playwrite

--- homework/vlad_shambaryan/Homework_27/task_3.py
+++ homework/vlad_shambaryan/Homework_27/task_3.py
@@ -1,10 +1,8 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 
-def test_color_change_button(page: Page):
+def test_color_change_button7(page: Page):
     page.goto("https://demoqa.com/dynamic-properties")
-    page.evaluate("window.scrollBy(0, 300);")
-    color_button = page.locator("#colorChange")
-    page.wait_for_function(
-        "element => getComputedStyle(element).color === 'rgb(220, 53, 69)'", arg=color_button.element_handle())
-    color_button.click()
+    red_color_button = page.locator("//button[contains(@class,'mt-4 text-danger btn btn-primary')]")
+    red_color_button.click()
+    expect(red_color_button).to_have_css("color", "rgb(220, 53, 69)")
