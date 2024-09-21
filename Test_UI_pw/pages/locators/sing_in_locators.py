9f616f0b6c7176c258a4e9422fa943a28f491c0b
Vladimir Shambaryan <132017930+Vladshambaryan@users.noreply.github.com>
2024-09-22 01:55:32 +0400

    Delete Test_UI_pw/pages/locators/sing_in_locators.py

--- Test_UI_pw/pages/locators/sing_in_locators.py
+++
@@ -1,7 +0,0 @@
-
-sing_loc = "(//a[contains(.,'Sign In')])[1]"
-email_field_loc = "#email"
-password_field_loc = '#pass'
-sing_btn_loc = '#send2'
-welcome_loc = "(//span[contains(.,'Welcome, Elon Mask!')])[1]"
-error_loc = "//div[contains(@data-ui-id,'message-error')]"
