b47ab6c2a06779722ffb8486b35367c39438842d
PC <skachat30913@gmail.com>
2024-09-23 23:40:42 +0400

    Docker

---
+++ homework/vlad_shambaryan/Homework_32/sep_23/openweather.py
@@ -0,0 +1,14 @@
+import requests
+
+city = 'Ереван'
+
+url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+
+weather_data = requests.get(url).json()
+
+temperature = round(weather_data['main']['temp'])
+temperature_feels = round(weather_data['main']['feels_like'])
+
+print('Сейчас в городе', city, str(temperature), '°C')
+print('Ощущается как', str(temperature_feels), '°C')
+print("hello Ереван!")
