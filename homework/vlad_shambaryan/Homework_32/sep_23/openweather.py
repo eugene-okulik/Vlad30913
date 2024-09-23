37b4a6699f6bcc89b9bc58a23d262068d3268d7f
PC <skachat30913@gmail.com>
2024-09-24 00:30:25 +0400

    Docker!

--- homework/vlad_shambaryan/Homework_32/sep_23/openweather.py
+++ homework/vlad_shambaryan/Homework_32/sep_23/openweather.py
@@ -3,7 +3,7 @@ import requests
 
 city = 'Ереван'
 url = ('https://api.openweathermap.org/data/2.5/'
-       'weather?q='+city+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347')
+       'weather?q=' +city+ '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347')
 weather_data = requests.get(url).json()
 temperature = round(weather_data['main']['temp'])
 temperature_feels = round(weather_data['main']['feels_like'])
